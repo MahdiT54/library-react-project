dce0f86676ac2c4c1f0e54dd0334e40c01623eba
Mahdi <mahditanzim2@gmail.com>
2023-08-13 08:13:24 -0400

    Nav & landing finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Nav from "./components/Nav.jsx";
+import Landing from "./components/Landing.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Featured from "./components/Featured.jsx";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faShoppingCart,
+  faTimes,
+  faBolt,
+  faBookOpen,
+  faTags,
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBars, faShoppingCart)
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
