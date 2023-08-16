d4099cd7c8e611d70aa31dc15799bb853d125f00
Mahdi <mahditanzim2@gmail.com>
2023-08-16 01:20:00 -0400

    bookInfo added, issue with routing to /books

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Nav from "./components/Nav.jsx";
 import Footer from "./components/Footer.jsx";
+import Nav from "./components/Nav.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Books from "./pages/Books.jsx";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo.jsx";
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home} />
-        <Route path="/books" component={Books} />
+        <Route path="/books" render={() => <Books books={books} />} />
+        <Route path="/books/1" render={() => <BookInfo books={books} />} />
         <Footer />
       </div>
     </Router>
