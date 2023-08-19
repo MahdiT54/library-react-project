9967dd9cab00af63a42ad77a79491d8f992b9b52
Mahdi <mahditanzim2@gmail.com>
2023-08-19 06:09:28 -0400

    logo update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../assets/Library.svg";
+import Logo from "../assets/Library.png";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import UndrawBooks from '../assets/Undraw_Books.svg';
+import React from "react";
+import UndrawBooks from "../assets/Undraw_Books.svg";
+import { Link as ScrollLink } from "react-scroll";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
-    return (
-        <section id="landing">
-            <header>
-                <div className="header__container">
-                    <div className="header__description">
-                        <h1>America's most awarded online library platform</h1>
-                        <h2>Find your dream book with <span className="purple">Library</span></h2>
-                        <Link to="#features">
-                            <button className="btn">Browse books</button>
-                        </Link>
-                    </div>
-                    <figure className="header__img--wrapper">
-                        <img src={UndrawBooks} alt="" />
-                    </figure>
-                </div>
-            </header>
-        </section>
-    );
-}
+  return (
+    <section id="landing">
+      <header>
+        <div className="header__container">
+          <div className="header__description">
+            <h1>America's most awarded online library platform</h1>
+            <h2>
+              Find your dream book with <span className="purple">Library</span>
+            </h2>
+            <ScrollLink to="features" smooth={true} duration={300}>
+              <button className="btn">Browse books</button>
+            </ScrollLink>
+          </div>
+          <figure className="header__img--wrapper">
+            <img src={UndrawBooks} alt="" />
+          </figure>
+        </div>
+      </header>
+    </section>
+  );
+};
 
 export default Landing;
