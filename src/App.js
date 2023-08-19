260e545ffb34a7c658f39bd04075a49bd37a9698
Mahdi <mahditanzim2@gmail.com>
2023-08-18 23:52:14 -0400

    Cart added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Home from "./pages/Home.jsx";
 import Books from "./pages/Books.jsx";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo.jsx";
+import Cart from "./pages/Cart.jsx";
+import React, { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
   return (
     <Router>
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home} />
-        <Route path="/books" render={() => <Books books={books} />} />
-        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route path="/books/:id" render={() => <BookInfo books={books} />} />
+        <Route path="/cart" render={() => <Cart books={books} />} />
         <Footer />
       </div>
     </Router>
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import {
   faTags,
   faStar,
   faStarHalfAlt,
+  faArrowLeft
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
@@ -23,14 +24,11 @@ library.add(
   faBookOpen,
   faTags,
   faStar,
-  faStarHalfAlt
+  faStarHalfAlt,
+  faArrowLeft
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 reportWebVitals();
